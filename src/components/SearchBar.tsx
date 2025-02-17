a179d0f2b5b6503c6d2115e135b9dae1f863cccd
abp437 <abp437@gmail.com>
2025-02-17 14:04:07 +0530

    Improvide the search bar experience

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,15 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { setSearchQuery } from "../app/searchSlice";
 import SearchIcon from "./icons/SearchIcon";
 
 const SearchBar: React.FC = () => {
   const [query, setQuery] = useState("");
+  const [isReset, setIsReset] = useState(false);
   const dispatch = useDispatch();
 
-  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(event.target.value);
-    dispatch(setSearchQuery(event.target.value));
+  const submitRequest = () => {
+    dispatch(setSearchQuery(query));
+  };
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const isSearchBlank: boolean = e.target.value.length === 0;
+    setQuery(e.target.value);
+    setIsReset(isSearchBlank);
+  };
+
+  useEffect(() => {
+    if (isReset) {
+      console.log("Search is made blank reset to initial results set");
+      submitRequest();
+    }
+  }, [isReset]);
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const isEnterPressed: boolean = e.key === "Enter";
+
+    if (isEnterPressed) {
+      submitRequest();
+    }
   };
 
   return (
@@ -19,10 +40,17 @@ const SearchBar: React.FC = () => {
           type="text"
           value={query}
           onChange={handleSearchChange}
+          onKeyDown={handleKeyDown}
           placeholder="Search news, topics & more..."
-          className="w-full pl-4 pr-8 py-2 border-1"
+          className="w-full pl-4 pr-8 py-2 border-1 focus:outline-none"
         />
-        <SearchIcon extraClasses="absolute top-2 right-2" />
+        <button
+          className="absolute p-2 top-0 right-0 bottom-0 cursor-pointer border-l-1"
+          aria-label="Search"
+          onClick={submitRequest}
+        >
+          <SearchIcon />
+        </button>
       </div>
     </div>
   );
